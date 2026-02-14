5b8dbb88810e8bd509ed7d48fb9a55fd7e2a60e8
Fedor Kazin <japanxt@ya.ru>
2026-02-14 17:08:19 +0500

    llm-exper: team_planets.php

--- team_planets.php
+++ team_planets.php
@@ -2,13 +2,6 @@
 
 include 'config.php';
 
-
-
-
-
-
-
-
 $title = $l_teamplanet_title;
 include("header.php");
 
@@ -16,29 +9,26 @@ if (checklogin()) {
     die();
 }
 
+$playerinfo = db()->fetch("SELECT * FROM ships WHERE email= :username", [
+    'username' => $username
+]);
 
-$res = $db->adoExecute("SELECT * FROM ships WHERE email='$username'");
-$playerinfo = $res->fields;
-
-if ($playerinfo[team] == 0) {
+if ($playerinfo['team'] == 0) {
     echo "<BR>$l_teamplanet_notally";
     echo "<BR><BR>";
-    
 
     include("footer.php");
-
     return;
 }
 
-
-$query = "SELECT * FROM planets WHERE corp=$playerinfo[team]";
+$query = "SELECT * FROM planets WHERE corp= :team";
 if (!empty($sort)) {
     $query .= " ORDER BY";
     if ($sort == "name") {
         $query .= " $sort ASC";
     } elseif (
-        $sort == "organics" || $sort == "ore" || $sort == "goods" || $sort == "energy" ||
-        $sort == "colonists" || $sort == "credits" || $sort == "fighters"
+    $sort == "organics" || $sort == "ore" || $sort == "goods" || $sort == "energy" ||
+    $sort == "colonists" || $sort == "credits" || $sort == "fighters"
     ) {
         $query .= " $sort DESC";
     } elseif ($sort == "torp") {
@@ -48,47 +38,38 @@ if (!empty($sort)) {
     }
 }
 
-$res = $db->adoExecute($query);
+$planets = db()->fetchAll($query, [
+    'team' => $playerinfo['team']
+]);
 
 bigtitle();
 
-
 echo "<BR>";
-echo "<B><A HREF=planet_report.php>$l_teamplanet_personal</A></B>";
+echo "<strong><A HREF=planet_report.php>$l_teamplanet_personal</A></strong>";
 echo "<BR>";
 echo "<BR>";
 
-
-$i = 0;
-if ($res) {
-    while (!$res->EOF) {
-        $planet[$i] = $res->fields;
-        $i++;
-
-        $res->Movenext();
-    }
-}
-
-$num_planets = $i;
+$num_planets = count($planets);
 if ($num_planets < 1) {
     echo "<BR>$l_teamplanet_noplanet";
 } else {
     echo "$l_pr_clicktosort<BR><BR>";
-    echo "<TABLE WIDTH=\"100%\" BORDER=0 CELLSPACING=0 CELLPADDING=2>";
-    echo "<TR BGCOLOR=\"$color_header\">";
-    echo "<TD><B><A HREF=team_planets.php?sort=sector>$l_sector</A></B></TD>";
-    echo "<TD><B><A HREF=team_planets.php?sort=name>$l_name</A></B></TD>";
-    echo "<TD><B><A HREF=team_planets.php?sort=ore>$l_ore</A></B></TD>";
-    echo "<TD><B><A HREF=team_planets.php?sort=organics>$l_organics</A></B></TD>";
-    echo "<TD><B><A HREF=team_planets.php?sort=goods>$l_goods</A></B></TD>";
-    echo "<TD><B><A HREF=team_planets.php?sort=energy>$l_energy</A></B></TD>";
-    echo "<TD><B><A HREF=team_planets.php?sort=colonists>$l_colonists</A></B></TD>";
-    echo "<TD><B><A HREF=team_planets.php?sort=credits>$l_credits</A></B></TD>";
-    echo "<TD><B><A HREF=team_planets.php?sort=fighters>$l_fighters</A></B></TD>";
-    echo "<TD><B><A HREF=team_planets.php?sort=torp>$l_torps</A></B></TD>";
-    echo "<TD><B>$l_base?</B></TD><TD><B>$l_selling?</B></TD>";
-    echo "<TD><B>Player</B></TD>";
+    echo "<TABLE class=\"table\">";
+    echo "<TR>";
+    echo "<TH><A HREF=team_planets.php?sort=sector>$l_sector</A></TH>";
+    echo "<TH><A HREF=team_planets.php?sort=name>$l_name</A></TH>";
+    echo "<TH><A HREF=team_planets.php?sort=ore>$l_ore</A></TH>";
+    echo "<TH><A HREF=team_planets.php?sort=organics>$l_organics</A></TH>";
+    echo "<TH><A HREF=team_planets.php?sort=goods>$l_goods</A></TH>";
+    echo "<TH><A HREF=team_planets.php?sort=energy>$l_energy</A></TH>";
+    echo "<TH><A HREF=team_planets.php?sort=colonists>$l_colonists</A></TH>";
+    echo "<TH><A HREF=team_planets.php?sort=credits>$l_credits</A></TH>";
+    echo "<TH><A HREF=team_planets.php?sort=fighters>$l_fighters</A></TH>";
+    echo "<TH><A HREF=team_planets.php?sort=torp>$l_torps</A></TH>";
+    echo "<TH>$l_base?</TH><TH>$l_selling?</TH>";
+    echo "<TH>Player</TH>";
     echo "</TR>";
+
     $total_organics = 0;
     $total_ore = 0;
     $total_goods = 0;
@@ -99,52 +80,51 @@ if ($num_planets < 1) {
     $total_torp = 0;
     $total_base = 0;
     $total_selling = 0;
-    $color = $color_line1;
-    for ($i = 0; $i < $num_planets; $i++) {
-        $total_organics += $planet[$i][organics];
-        $total_ore += $planet[$i][ore];
-        $total_goods += $planet[$i][goods];
-        $total_energy += $planet[$i][energy];
-        $total_colonists += $planet[$i][colonists];
-        $total_credits += $planet[$i][credits];
-        $total_fighters += $planet[$i][fighters];
-        $total_torp += $planet[$i][torps];
-        if ($planet[$i][base] == "Y") {
+
+    foreach ($planets as $planet) {
+        $total_organics += $planet['organics'];
+        $total_ore += $planet['ore'];
+        $total_goods += $planet['goods'];
+        $total_energy += $planet['energy'];
+        $total_colonists += $planet['colonists'];
+        $total_credits += $planet['credits'];
+        $total_fighters += $planet['fighters'];
+        $total_torp += $planet['torps'];
+
+        if ($planet['base'] == "Y") {
             $total_base += 1;
         }
-        if ($planet[$i][sells] == "Y") {
+        if ($planet['sells'] == "Y") {
             $total_selling += 1;
         }
-        if (empty($planet[$i][name])) {
-            $planet[$i][name] = "$l_unnamed";
-        }
-        $owner = $planet[$i][owner];
-        $res = $db->adoExecute("SELECT character_name FROM ships WHERE ship_id=$owner");
-        $player = $res->fields[character_name];
 
-        echo "<TR BGCOLOR=\"$color\">";
-        echo "<TD><A HREF=rsmove.php?engage=1&destination=" . $planet[$i][sector_id] . ">" . $planet[$i][sector_id] . "</A></TD>";
-        echo "<TD>" . $planet[$i][name]              . "</TD>";
-        echo "<TD>" . NUMBER($planet[$i][ore])       . "</TD>";
-        echo "<TD>" . NUMBER($planet[$i][organics])  . "</TD>";
-        echo "<TD>" . NUMBER($planet[$i][goods])     . "</TD>";
-        echo "<TD>" . NUMBER($planet[$i][energy])    . "</TD>";
-        echo "<TD>" . NUMBER($planet[$i][colonists]) . "</TD>";
-        echo "<TD>" . NUMBER($planet[$i][credits])   . "</TD>";
-        echo "<TD>" . NUMBER($planet[$i][fighters])  . "</TD>";
-        echo "<TD>" . NUMBER($planet[$i][torps])     . "</TD>";
-        echo "<TD>" . ($planet[$i][base] == 'Y' ? "$l_yes" : "$l_no") . "</TD>";
-        echo "<TD>" . ($planet[$i][sells] == 'Y' ? "$l_yes" : "$l_no") . "</TD>";
-        echo "<TD>" . $player                        . "</TD>";
+        if (empty($planet['name'])) {
+            $planet['name'] = "$l_unnamed";
+        }
+
+        $owner = $planet['owner'];
+        $player = db()->fetch("SELECT character_name FROM ships WHERE ship_id= :owner", [
+            'owner' => $owner
+        ]);
+
+        echo "<TR>";
+        echo "<TD><A HREF=rsmove.php?engage=1&destination=" . $planet['sector_id'] . ">" . $planet['sector_id'] . "</A></TD>";
+        echo "<TD>" . $planet['name'] . "</TD>";
+        echo "<TD>" . NUMBER($planet['ore']) . "</TD>";
+        echo "<TD>" . NUMBER($planet['organics']) . "</TD>";
+        echo "<TD>" . NUMBER($planet['goods']) . "</TD>";
+        echo "<TD>" . NUMBER($planet['energy']) . "</TD>";
+        echo "<TD>" . NUMBER($planet['colonists']) . "</TD>";
+        echo "<TD>" . NUMBER($planet['credits']) . "</TD>";
+        echo "<TD>" . NUMBER($planet['fighters']) . "</TD>";
+        echo "<TD>" . NUMBER($planet['torps']) . "</TD>";
+        echo "<TD>" . ($planet['base'] == 'Y' ? "$l_yes" : "$l_no") . "</TD>";
+        echo "<TD>" . ($planet['sells'] == 'Y' ? "$l_yes" : "$l_no") . "</TD>";
+        echo "<TD>" . $player['character_name'] . "</TD>";
         echo "</TR>";
-
-        if ($color == $color_line1) {
-            $color = $color_line2;
-        } else {
-            $color = $color_line1;
-        }
     }
-    echo "<TR BGCOLOR=\"$color\">";
+
+    echo "<TR>";
     echo "<TD></TD>";
     echo "<TD>$l_pr_totals</TD>";
     echo "<TD>" . NUMBER($total_ore) . "</TD>";
@@ -163,7 +143,5 @@ if ($num_planets < 1) {
 }
 
 echo "<BR><BR>";
-
-
 
 include("footer.php");
